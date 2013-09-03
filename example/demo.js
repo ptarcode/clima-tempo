f72832b62bc099e1991691e82f4bcbe6f6bd77d0
Paulo de Tarço <paulopjazz@hotmail.com>
2013-09-02 21:14:05 -0300

    Alterações no readme

--- example/demo.js
+++ example/demo.js
@@ -5,11 +5,11 @@ var callback = function(error, result) {
         console.error(error);
     }
     else{
-    	console.log(result);
+    	console.log(result+'==========================');
     }
 }
 //type = Default or  Extended
-cli.days('3156',"Extended",callback);
-cli.nowFromPage('3156',callback);
-cli.fullFromPage('3156',callback);
+// cli.days('3156',"Extended",callback);
+// cli.nowFromPage('3156',callback);
+// cli.fullFromPage('3156',callback);
 cli.trendsFromPage('3156',callback);
